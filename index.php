36e4f11286d314810edfaa40c5011ef027c2a836
blinkdeploys <93453656+blinkdeploys@users.noreply.github.com>
2022-02-06 13:37:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php 
 
+function findMatches($rgx="") {
+  
+  if ($rgx == null) die("Pattern string not found");
+  
+  // the file location
+  $fileLoc = "parse.txt";
+  
+  // build the pattern, square braces
+  $rgx = "/\[" + $rgx + "\]/";
+
+  // open the file
+  $filePtr = fopen($fileLoc, "r") or die("404!");
+
+  // read in the file
+  $fileContents = "";
+  
+  // while end of file has not been reached...
+  while(!feof($filePtr) {
+    // read one line in...
+    $fileContents += fgetc($filePtr);
+  }
+  
+  // find all occurrences of the input string [$rgx]
+  if (preg_match_all($rgx, $fileContents, $matches)) {
+    
+    // show all the matches to the console
+    var_dump($matches);
+    
+  }
+        
+  // close the file pointer
+  fclose($filePtr);
+  
+      
+}
+        
+findMatches("lorem ispum");
+  
 ?>
